0709e7ed62dcc7f947234f480ccab2cb492fb5a2
Karol Martyński <karolmartynski@macbook-pro-karol.home>
2022-05-15 22:58:23 +0200

    Updated .gitignore; added factory pattern with tests

---
+++ creational/factory.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+import uuid
+
+
+MESSAGE_MAPPER = {
+    "welcome": "Greetings",
+    "farewell": "Goodbye",
+    "plans": "What are you going to do today?"
+}
+
+
+class MessageGenerator:
+    def __init__(self, message_name: str):
+        self.message_name = message_name
+
+    def create_message(self) -> dict:
+        type_of_message = MESSAGE_MAPPER[self.message_name]
+        message = {
+            "header": self._create_header(),
+            "body": type_of_message
+        }
+        return message
+
+    def _create_header(self) -> dict:
+        header = {
+            "id": uuid.uuid4(),
+            "time": datetime.utcnow(),
+
+        }
+        return header
+
+
+class MessageFactory:
+    def __new__(cls, message_name, *args, **kwargs):
+        return MessageGenerator(message_name=message_name).create_message()
+
+
+if __name__ == "__main__":
+    first_message = MessageFactory("welcome")
+    second_message = MessageFactory("plans")
+    third_message = MessageFactory("farewell")
+
+    print(first_message)
+    print(second_message)
+    print(third_message)
---
+++ tests/test_factory.py
@@ -0,0 +1,30 @@
+from pytest import mark, fixture, raises
+
+from creational.factory import MessageFactory
+
+
+@fixture
+def example_message():
+    factory = MessageFactory(message_name="welcome")
+    return factory
+
+
+class TestMessageFactory:
+
+    @mark.parametrize(
+        "message_input, message_response",
+        [
+            ("welcome", "Greetings"),
+            ("farewell", "Goodbye"),
+            ("plans", "What are you going to do today?")
+        ]
+    )
+    def test_should_return_message_body(self, message_input, message_response):
+        factory = MessageFactory(message_name=message_input)
+        assert factory.get("body") == message_response
+
+    def test_should_return_message_keys(self, example_message):
+        assert "header", "body" in example_message
+
+    def test_should_return_message_generator_type(self, example_message):
+        assert isinstance(example_message, dict)
