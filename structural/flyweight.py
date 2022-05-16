70381b0a611fa3c41512256d7bc43e46b2a88003
Karol Martyński <karolmartynski@macbook-pro-karol.home>
2022-05-16 23:01:10 +0200

    Added flyweight pattern

---
+++ structural/flyweight.py
@@ -0,0 +1,43 @@
+class Product:
+
+    def products(self, product_name):
+        return "Flyweight pattern[% s]" % product_name
+
+
+class FlyweightFactory:
+    flyweight_products = {}
+
+    def __new__(cls, name, product_type):
+        try:
+            product_id = cls.flyweight_products[product_type]
+        except KeyError:
+            product_id = object.__new__(cls)
+            cls.flyweight_products[product_type] = product_id
+        return product_id
+
+    def set_product_data(self, product_info):
+        product_instance = Product()
+        self.product_data = product_instance.products(product_info)
+
+    def get_product_data(self):
+        return self.product_data
+
+
+if __name__ == '__main__':
+    product_data = (('Banana', 'Fruit'), ('Snickers', 'Sweet'), ('Chips', 'Sweet'), ('Apple', 'Fruit'), ("Tomato", "Veggie"))
+    list_of_products = []
+
+    for i in product_data:
+        item = FlyweightFactory(i[0], i[1])
+        item.set_product_data(i[1])
+        list_of_products.append(item)
+
+    print(list_of_products)
+    print(set(list_of_products))
+
+    assert list_of_products != set(list_of_products)
+
+    for j in list_of_products:
+        print(j)
+        print(" id = " + str(id(j)))
+        print(j.get_product_data())
