e854bc252c59436f3cfcb2f8be243ed1b4ee163f
Karol Martyński <karolmartynski@macbook-pro-karol.home>
2022-05-17 17:10:16 +0200

    Added tests for chain_of_responsibility

--- tests/test_chain_of_responsibility.py
+++ tests/test_chain_of_responsibility.py
@@ -1,4 +1,4 @@
-from pytest import mark, fixture
+from pytest import mark, raises
 
 from behavioral.chain_of_responsibility import (
     RandomStore,
@@ -7,24 +7,61 @@ from behavioral.chain_of_responsibility import (
     ErrorCodeHandler
 )
 
-visitor = IsStoreOpenHandler()
-maintenance = MaintenanceHandler()
-error_code = ErrorCodeHandler()
+is_store_open_handler = IsStoreOpenHandler()
+maintenance_handler = MaintenanceHandler()
+error_code_handler = ErrorCodeHandler()
 
 
-@fixture
-def random_store():
-    store_status = RandomStore(
-        is_store_open=True,
-        maintenance=False,
-        error_code="unknown"
+class TestChainOfResponsibility:
+
+    @mark.parametrize(
+        "is_store_open, maintenance, error_code, exception",
+        [
+            (False, False, "error", "Store is closed!"),
+            (True, True, "error", "Store is currently maintained"),
+            (True, False, "error", "An unknown error has occurred!")
+        ]
     )
-    return store_status
+    def test_should_return_error_for_each_handler(
+            self,
+            is_store_open,
+            maintenance,
+            error_code,
+            exception,
+    ):
+        store_status = RandomStore(
+            is_store_open=is_store_open,
+            maintenance=maintenance,
+            error_code=error_code
+        )
+        is_store_open_handler.set_successor(maintenance_handler).set_successor(error_code_handler)
 
-@fixture
-def random_chain():
-    visitor.set_successor(maintenance).set_successor(error_code)
+        with raises(Exception):
+            print(is_store_open_handler.handle(store_status))
 
-@fixture
-def random_chain_2():
-    maintenance.set_successor(visitor).set_successor(error_code)
+
+    @mark.parametrize(
+        "handler_1, handler_2, handler_3",
+        [
+            (IsStoreOpenHandler(), MaintenanceHandler(), ErrorCodeHandler()),
+            (IsStoreOpenHandler(), ErrorCodeHandler(), MaintenanceHandler()),
+            (MaintenanceHandler(), IsStoreOpenHandler(), ErrorCodeHandler()),
+            (MaintenanceHandler(), ErrorCodeHandler(), IsStoreOpenHandler()),
+            (ErrorCodeHandler(), MaintenanceHandler(), IsStoreOpenHandler()),
+            (ErrorCodeHandler(), IsStoreOpenHandler(), MaintenanceHandler()),
+        ]
+    )
+    def test_should_return_none_if_no_exception_with_different_handler_configuration(
+            self,
+            handler_1,
+            handler_2,
+            handler_3
+    ):
+        store_status = RandomStore(
+            is_store_open=True,
+            maintenance=False,
+            error_code="unknown"
+        )
+
+        handler_1.set_successor(handler_2).set_successor(handler_3)
+        assert handler_1.handle(store_status) is None
